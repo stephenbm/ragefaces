bbdfdb3eba378a7f173fe3b89feec5259a257bc3
Stephen Breyer-Menke <steve.bm@gmail.com>
2013-11-29 08:01:18 +0200

    Add new images to the asset pipeline, bump version

--- lib/ragefaces/engine.rb
+++ lib/ragefaces/engine.rb
@@ -1,7 +1,8 @@
 module Ragefaces
   class Engine < Rails::Engine
       initializer "ragefaces.assets.precompile" do |app|
-        app.config.assets.precompile += %w(rage-sprite.png)
+        app.config.assets.precompile += %w(*.png)
+        app.config.assets.precompile += %w(*.gif)
       end
   end
 end
--- lib/ragefaces/version.rb
+++ lib/ragefaces/version.rb
@@ -1,3 +1,3 @@
 module Ragefaces
-  VERSION = "0.0.3"
+  VERSION = "0.0.3.1"
 end
